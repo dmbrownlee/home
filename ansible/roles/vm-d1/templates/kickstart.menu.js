2f5f38e2f9ec78e37ac12bc04e18f87067952087
dmbrownlee <dmbrownlee@users.noreply.github.com>
2017-02-02 03:30:43 -0800

    Go back to using old ethX interface names so we don't have to worry about interface name and MAC address differences between the real and the virtual hardware.

--- ansible/roles/vm-d1/templates/kickstart.menu.js
+++ ansible/roles/vm-d1/templates/kickstart.menu.js
@@ -3,7 +3,7 @@
 LABEL d2
   MENU LABEL ^Install d2.home.test
   kernel centos7/vmlinuz
-  append initrd=centos7/initrd.img inst.repo=ftp://{{ ansible_default_ipv4.address }}/pub devfs=nomount inst.ks=https://raw.githubusercontent.com/dmbrownlee/home.test/master/d2-ks.cfg ip=dhcp
+  append initrd=centos7/initrd.img inst.repo=ftp://{{ ansible_default_ipv4.address }}/pub devfs=nomount inst.ks=https://raw.githubusercontent.com/dmbrownlee/home.test/master/d2-ks.cfg ip=dhcp net.ifnames=0
 
 LABEL MainMenu
   MENU LABEL ^Return to Main Menu
