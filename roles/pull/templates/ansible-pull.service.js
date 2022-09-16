5ce5aec56710f52e76489c999c9cba7635877fb2
D. M. Brownlee <dmbrownlee@users.noreply.github.com>
2022-09-16 09:14:52 -0700

    Fix template tags.

--- roles/pull/templates/ansible-pull.service.js
+++ roles/pull/templates/ansible-pull.service.js
@@ -4,7 +4,7 @@ Documentation=man:ansible-pull(1)
 
 [Service]
 Type=oneshot
-ExecStart=/usr/bin/ansible-pull --vault-password-file /home/{% cfg_account.name %}/.vaultpw -o -U https://github.com/dmbrownlee/home.git -C release
+ExecStart=/usr/bin/ansible-pull --vault-password-file /home/{{ cfg_account.name }}/.vaultpw -o -U https://github.com/dmbrownlee/home.git -C release
 # performance options
 Nice=19
 IOSchedulingClass=best-effort
