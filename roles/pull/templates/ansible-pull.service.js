7e80199030b8454a97f2cc80275106c6d83110ec
D. M. Brownlee <dmbrownlee@users.noreply.github.com>
2022-09-19 05:32:42 -0700

    Use the system service.manager rather than a user service.manager to avoid capability issues with ansible become.

--- roles/pull/templates/ansible-pull.service.js
+++ roles/pull/templates/ansible-pull.service.js
@@ -4,11 +4,9 @@ Documentation=man:ansible-pull(1)
 
 [Service]
 Type=oneshot
+WorkingDirectory=/home/{{ cfg_account.name }}
+User={{ cfg_account.name }}
 ExecStart=/usr/bin/ansible-pull --vault-password-file /home/{{ cfg_account.name }}/.vaultpw -o -U https://github.com/dmbrownlee/home.git -C release
-# performance options
-Nice=19
-IOSchedulingClass=best-effort
-IOSchedulingPriority=7
 
 # hardening options
 #  details: https://www.freedesktop.org/software/systemd/man/systemd.exec.html
@@ -16,16 +14,16 @@ IOSchedulingPriority=7
 #  no PrivateNetwork for mail deliviery
 #  no NoNewPrivileges for third party rotate scripts
 #  no RestrictSUIDSGID for creating setgid directories
-LockPersonality=true
-MemoryDenyWriteExecute=true
-PrivateDevices=true
-PrivateTmp=true
-ProtectClock=true
-ProtectControlGroups=true
-ProtectHostname=true
-ProtectKernelLogs=true
-ProtectKernelModules=true
-ProtectKernelTunables=true
-ProtectSystem=full
-RestrictNamespaces=true
-RestrictRealtime=true
+# LockPersonality=true
+# MemoryDenyWriteExecute=true
+# PrivateDevices=true
+# PrivateTmp=true
+# ProtectClock=true
+# ProtectControlGroups=true
+# ProtectHostname=true
+# ProtectKernelLogs=true
+# ProtectKernelModules=true
+# ProtectKernelTunables=true
+# ProtectSystem=full
+# RestrictNamespaces=true
+# RestrictRealtime=true
