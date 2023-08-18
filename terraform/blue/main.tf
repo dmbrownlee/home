c1d914725648ec2d087f03006bf7eadfae7c259b
D. M. Brownlee <dmbrownlee@users.noreply.github.com>
2023-08-17 22:40:01 -0700

    Initial check-in for terraform configs for blue vms.

---
+++ terraform/blue/main.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "telmate/proxmox"
+      version = "2.9.14"
+    }
+    ansible = {
+      source  = "ansible/ansible"
+      version = "1.1.0"
+    }
+  }
+}
---
+++ terraform/blue/vms.tf
@@ -0,0 +1,284 @@
+# proxmox_vm_qemu.git1:
+resource "proxmox_vm_qemu" "git-blue" {
+    agent                  = 1
+    balloon                = 0
+    bios                   = "ovmf"
+    boot                   = "order=scsi0;ide2;net0"
+    clone                  = "debian12-template"
+    cores                  = 1
+    cpu                    = "x86-64-v2-AES"
+    define_connection_info = false
+    force_create           = false
+    full_clone             = true
+    hotplug                = "network,disk,usb"
+    vmid                   = "300"
+    kvm                    = true
+    memory                 = 2048
+    name                   = "git-blue"
+    numa                   = false
+    onboot                 = true
+    oncreate               = false
+    qemu_os                = "l26"
+    scsihw                 = "virtio-scsi-single"
+    sockets                = 1
+    tablet                 = true
+    target_node            = "pve1-mgt"
+    vcpus                  = 0
+
+    disk {
+        backup             = true
+        cache              = "none"
+        file               = "300/vm-300-disk-1.qcow2"
+        iops               = 0
+        iops_max           = 0
+        iops_max_length    = 0
+        iops_rd            = 0
+        iops_rd_max        = 0
+        iops_rd_max_length = 0
+        iops_wr            = 0
+        iops_wr_max        = 0
+        iops_wr_max_length = 0
+        iothread           = 1
+        mbps               = 0
+        mbps_rd            = 0
+        mbps_rd_max        = 0
+        mbps_wr            = 0
+        mbps_wr_max        = 0
+        replicate          = 0
+        size               = "32G"
+        slot               = 0
+        ssd                = 0
+        storage            = "truenas1"
+        type               = "scsi"
+        volume             = "truenas1:300/vm-300-disk-1.qcow2"
+    }
+
+    network {
+        bridge    = "vmbr0"
+        firewall  = true
+        link_down = false
+        macaddr   = "CA:FE:01:00:01:00"
+        model     = "virtio"
+        mtu       = 0
+        queues    = 0
+        rate      = 0
+        tag       = 30
+    }
+}
+
+# proxmox_vm_qemu.ansible1:
+resource "proxmox_vm_qemu" "ansible-blue" {
+    agent                  = 1
+    balloon                = 0
+    bios                   = "ovmf"
+    boot                   = "order=scsi0;ide2;net0"
+    clone                  = "debian12-template"
+    cores                  = 8
+    cpu                    = "x86-64-v2-AES"
+    define_connection_info = false
+    force_create           = false
+    full_clone             = true
+    hotplug                = "network,disk,usb"
+    vmid                   = "301"
+    kvm                    = true
+    memory                 = 8192
+    name                   = "ansible-blue"
+    numa                   = false
+    onboot                 = true
+    oncreate               = false
+    qemu_os                = "l26"
+    scsihw                 = "virtio-scsi-single"
+    sockets                = 1
+    tablet                 = true
+    target_node            = "pve1-mgt"
+    vcpus                  = 0
+
+    disk {
+        backup             = true
+        cache              = "none"
+        discard            = "on"
+        file               = "301/vm-301-disk-1.qcow2"
+        iops               = 0
+        iops_max           = 0
+        iops_max_length    = 0
+        iops_rd            = 0
+        iops_rd_max        = 0
+        iops_rd_max_length = 0
+        iops_wr            = 0
+        iops_wr_max        = 0
+        iops_wr_max_length = 0
+        iothread           = 1
+        mbps               = 0
+        mbps_rd            = 0
+        mbps_rd_max        = 0
+        mbps_wr            = 0
+        mbps_wr_max        = 0
+        replicate          = 0
+        size               = "60G"
+        slot               = 0
+        ssd                = 1
+        storage            = "truenas1"
+        type               = "scsi"
+        volume             = "truenas1:301/vm-301-disk-1.qcow2"
+    }
+
+    network {
+        bridge    = "vmbr0"
+        firewall  = true
+        link_down = false
+        macaddr   = "CA:FE:01:00:02:00"
+        model     = "virtio"
+        mtu       = 0
+        queues    = 0
+        rate      = 0
+        tag       = 30
+    }
+}
+
+# proxmox_vm_qemu.netbox1:
+resource "proxmox_vm_qemu" "netbox-blue" {
+    agent                  = 1
+    balloon                = 0
+    bios                   = "ovmf"
+    boot                   = "order=scsi0;ide2;net0"
+    clone                  = "debian12-template"
+    cores                  = 4
+    cpu                    = "host"
+    define_connection_info = false
+    force_create           = false
+    full_clone             = true
+    hotplug                = "network,disk,usb"
+    vmid                   = "302"
+    kvm                    = true
+    memory                 = 4096
+    name                   = "netbox-blue"
+    numa                   = false
+    onboot                 = true
+    oncreate               = false
+    qemu_os                = "l26"
+    scsihw                 = "virtio-scsi-single"
+    sockets                = 1
+    tablet                 = true
+    target_node            = "pve1-mgt"
+    vcpus                  = 0
+
+    disk {
+        backup             = true
+        cache              = "none"
+        file               = "302/vm-302-disk-1.qcow2"
+        iops               = 0
+        iops_max           = 0
+        iops_max_length    = 0
+        iops_rd            = 0
+        iops_rd_max        = 0
+        iops_rd_max_length = 0
+        iops_wr            = 0
+        iops_wr_max        = 0
+        iops_wr_max_length = 0
+        iothread           = 1
+        mbps               = 0
+        mbps_rd            = 0
+        mbps_rd_max        = 0
+        mbps_wr            = 0
+        mbps_wr_max        = 0
+        replicate          = 0
+        size               = "32G"
+        slot               = 0
+        ssd                = 0
+        storage            = "truenas1"
+        type               = "scsi"
+        volume             = "truenas1:302/vm-302-disk-1.qcow2"
+    }
+
+    network {
+        bridge    = "vmbr0"
+        firewall  = true
+        link_down = false
+        macaddr   = "CA:FE:01:00:03:00"
+        model     = "virtio"
+        mtu       = 0
+        queues    = 0
+        rate      = 0
+        tag       = 30
+    }
+}
+
+# proxmox_vm_qemu.pxe1:
+resource "proxmox_vm_qemu" "pxe-blue" {
+    agent                  = 1
+    balloon                = 0
+    bios                   = "ovmf"
+    boot                   = "order=scsi0;ide2;net0"
+    clone                  = "debian12-template"
+    cores                  = 2
+    cpu                    = "x86-64-v2-AES"
+    define_connection_info = false
+    force_create           = false
+    full_clone             = true
+    hotplug                = "network,disk,usb"
+    vmid                   = "303"
+    kvm                    = true
+    memory                 = 2048
+    name                   = "pxe-blue"
+    numa                   = false
+    onboot                 = true
+    oncreate               = false
+    qemu_os                = "l26"
+    scsihw                 = "virtio-scsi-single"
+    sockets                = 1
+    tablet                 = true
+    target_node            = "pve1-mgt"
+    vcpus                  = 0
+
+    disk {
+        backup             = true
+        cache              = "none"
+        discard            = "on"
+        file               = "303/vm-303-disk-1.qcow2"
+        iops               = 0
+        iops_max           = 0
+        iops_max_length    = 0
+        iops_rd            = 0
+        iops_rd_max        = 0
+        iops_rd_max_length = 0
+        iops_wr            = 0
+        iops_wr_max        = 0
+        iops_wr_max_length = 0
+        iothread           = 1
+        mbps               = 0
+        mbps_rd            = 0
+        mbps_rd_max        = 0
+        mbps_wr            = 0
+        mbps_wr_max        = 0
+        replicate          = 0
+        size               = "32G"
+        slot               = 0
+        ssd                = 1
+        storage            = "truenas1"
+        type               = "scsi"
+        volume             = "truenas1:303/vm-303-disk-1.qcow2"
+    }
+
+    network {
+        bridge    = "vmbr0"
+        firewall  = true
+        link_down = false
+        macaddr   = "CA:FE:01:00:04:00"
+        model     = "virtio"
+        mtu       = 0
+        queues    = 0
+        rate      = 0
+        tag       = 30
+    }
+    network {
+        bridge    = "vmbr0"
+        firewall  = true
+        link_down = false
+        macaddr   = "CA:FE:01:00:04:01"
+        model     = "virtio"
+        mtu       = 0
+        queues    = 0
+        rate      = 0
+        tag       = 10
+    }
+}
