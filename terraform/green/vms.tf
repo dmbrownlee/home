ae8f8cb06a59c251fb50cd6bd335179fd542dc22
D. M. Brownlee <dmbrownlee@users.noreply.github.com>
2023-08-24 16:40:48 -0700

    Clean up resource names, VM IDs, MAC addresses, and add registry2.

--- terraform/green/vms.tf
+++ terraform/green/vms.tf
@@ -1,5 +1,5 @@
 # proxmox_vm_qemu.ansible2:
-resource "proxmox_vm_qemu" "ansible-green" {
+resource "proxmox_vm_qemu" "ansible2" {
     agent                  = 1
     balloon                = 0
     bios                   = "ovmf"
@@ -11,10 +11,10 @@ resource "proxmox_vm_qemu" "ansible-green" {
     force_create           = false
     full_clone             = false
     hotplug                = "network,disk,usb"
-    vmid                   = "201"
+    vmid                   = "200"
     kvm                    = true
     memory                 = 8192
-    name                   = "ansible-green"
+    name                   = "ansible2"
     numa                   = false
     onboot                 = true
     oncreate               = true
@@ -29,7 +29,7 @@ resource "proxmox_vm_qemu" "ansible-green" {
         backup             = true
         cache              = "none"
         discard            = "on"
-        file               = "201/vm-201-disk-1.qcow2"
+        file               = "200/vm-200-disk-1.qcow2"
         iops               = 0
         iops_max           = 0
         iops_max_length    = 0
@@ -51,24 +51,24 @@ resource "proxmox_vm_qemu" "ansible-green" {
         ssd                = 1
         storage            = "truenas1"
         type               = "scsi"
-        volume             = "truenas1:201/vm-201-disk-1.qcow2"
+        volume             = "truenas1:200/vm-200-disk-1.qcow2"
     }
 
     network {
         bridge    = "vmbr0"
         firewall  = true
         link_down = false
-        macaddr   = "CA:FE:02:00:02:00"
+        macaddr   = "CA:FE:01:02:01:01"
         model     = "virtio"
         mtu       = 0
         queues    = 0
         rate      = 0
-        tag       = 10
+        tag       = 30
     }
 }
 
 # proxmox_vm_qemu.git2:
-resource "proxmox_vm_qemu" "git-green" {
+resource "proxmox_vm_qemu" "git2" {
     agent                  = 1
     balloon                = 0
     bios                   = "ovmf"
@@ -80,10 +80,10 @@ resource "proxmox_vm_qemu" "git-green" {
     force_create           = false
     full_clone             = false
     hotplug                = "network,disk,usb"
-    vmid                   = "200"
+    vmid                   = "201"
     kvm                    = true
     memory                 = 2048
-    name                   = "git-green"
+    name                   = "git2"
     numa                   = false
     onboot                 = true
     oncreate               = true
@@ -97,7 +97,7 @@ resource "proxmox_vm_qemu" "git-green" {
     disk {
         backup             = true
         cache              = "none"
-        file               = "200/vm-200-disk-1.qcow2"
+        file               = "201/vm-201-disk-1.qcow2"
         iops               = 0
         iops_max           = 0
         iops_max_length    = 0
@@ -119,24 +119,24 @@ resource "proxmox_vm_qemu" "git-green" {
         ssd                = 0
         storage            = "truenas1"
         type               = "scsi"
-        volume             = "truenas1:200/vm-200-disk-1.qcow2"
+        volume             = "truenas1:201/vm-201-disk-1.qcow2"
     }
 
     network {
         bridge    = "vmbr0"
         firewall  = true
         link_down = false
-        macaddr   = "CA:FE:02:00:01:00"
+        macaddr   = "CA:FE:01:02:02:01"
         model     = "virtio"
         mtu       = 0
         queues    = 0
         rate      = 0
-        tag       = 10
+        tag       = 30
     }
 }
 
 # proxmox_vm_qemu.netbox2:
-resource "proxmox_vm_qemu" "netbox-green" {
+resource "proxmox_vm_qemu" "netbox2" {
     agent                  = 1
     balloon                = 0
     bios                   = "ovmf"
@@ -151,7 +151,7 @@ resource "proxmox_vm_qemu" "netbox-green" {
     vmid                   = "202"
     kvm                    = true
     memory                 = 4096
-    name                   = "netbox-green"
+    name                   = "netbox2"
     numa                   = false
     onboot                 = true
     oncreate               = true
@@ -194,17 +194,17 @@ resource "proxmox_vm_qemu" "netbox-green" {
         bridge    = "vmbr0"
         firewall  = true
         link_down = false
-        macaddr   = "CA:FE:02:00:03:00"
+        macaddr   = "CA:FE:01:02:03:01"
         model     = "virtio"
         mtu       = 0
         queues    = 0
         rate      = 0
-        tag       = 10
+        tag       = 30
     }
 }
 
 # proxmox_vm_qemu.pxe2:
-resource "proxmox_vm_qemu" "pxe-green" {
+resource "proxmox_vm_qemu" "pxe2" {
     agent                  = 1
     balloon                = 0
     bios                   = "ovmf"
@@ -219,7 +219,7 @@ resource "proxmox_vm_qemu" "pxe-green" {
     vmid                   = "203"
     kvm                    = true
     memory                 = 2048
-    name                   = "pxe-green"
+    name                   = "pxe2"
     numa                   = false
     onboot                 = true
     oncreate               = true
@@ -263,18 +263,98 @@ resource "proxmox_vm_qemu" "pxe-green" {
         bridge    = "vmbr0"
         firewall  = true
         link_down = false
-        macaddr   = "CA:FE:02:00:04:00"
+        macaddr   = "CA:FE:01:02:04:01"
+        model     = "virtio"
+        mtu       = 0
+        queues    = 0
+        rate      = 0
+        tag       = 30
+    }
+    network {
+        bridge    = "vmbr0"
+        firewall  = true
+        link_down = false
+        macaddr   = "CA:FE:01:02:04:02"
         model     = "virtio"
         mtu       = 0
         queues    = 0
         rate      = 0
         tag       = 10
     }
+}
+
+# proxmox_vm_qemu.registry2:
+resource "proxmox_vm_qemu" "registry2" {
+    agent                  = 1
+    balloon                = 0
+    bios                   = "ovmf"
+    boot                   = "order=scsi0;ide2;net0"
+    clone                  = "debian12-template"
+    cores                  = 2
+    cpu                    = "x86-64-v2-AES"
+    define_connection_info = false
+    force_create           = false
+    full_clone             = false
+    hotplug                = "network,disk,usb"
+    vmid                   = "204"
+    kvm                    = true
+    memory                 = 2048
+    name                   = "registry2"
+    numa                   = false
+    onboot                 = true
+    oncreate               = true
+    qemu_os                = "l26"
+    scsihw                 = "virtio-scsi-single"
+    sockets                = 1
+    tablet                 = true
+    target_node            = "pve2-mgt"
+    vcpus                  = 0
+
+    disk {
+        backup             = true
+        cache              = "none"
+        discard            = "on"
+        file               = "204/vm-204-disk-1.qcow2"
+        iops               = 0
+        iops_max           = 0
+        iops_max_length    = 0
+        iops_rd            = 0
+        iops_rd_max        = 0
+        iops_rd_max_length = 0
+        iops_wr            = 0
+        iops_wr_max        = 0
+        iops_wr_max_length = 0
+        iothread           = 1
+        mbps               = 0
+        mbps_rd            = 0
+        mbps_rd_max        = 0
+        mbps_wr            = 0
+        mbps_wr_max        = 0
+        replicate          = 0
+        size               = "32G"
+        slot               = 0
+        ssd                = 1
+        storage            = "truenas1"
+        type               = "scsi"
+        volume             = "truenas1:204/vm-204-disk-1.qcow2"
+    }
+
     network {
         bridge    = "vmbr0"
         firewall  = true
         link_down = false
-        macaddr   = "CA:FE:02:00:04:01"
+        macaddr   = "CA:FE:01:02:05:01"
+        model     = "virtio"
+        mtu       = 0
+        queues    = 0
+        rate      = 0
+        tag       = 30
+    }
+    network {
+        bridge    = "vmbr0"
+        firewall  = true
+        link_down = false
+        macaddr   = "CA:FE:01:02:05:02"
         model     = "virtio"
         mtu       = 0
         queues    = 0
