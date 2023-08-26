a58b013d0b10b05215c3a288eebdef545f000380
D. M. Brownlee <dmbrownlee@users.noreply.github.com>
2023-08-26 10:15:18 -0700

    Update VM IDs and MAC addresses for k8s nodes.

--- terraform/k8s/vms.tf
+++ terraform/k8s/vms.tf
@@ -1,7 +1,7 @@
 resource "proxmox_vm_qemu" "k8s_node1" {
     agent                  = 1
     bios                   = "ovmf"
-    boot                   = "order=ide2;scsi0;net0"
+    boot                   = "order=scsi0;ide2;net0"
     clone                  = "debian12-template"
     cores                  = 2
     cpu                    = "host"
@@ -9,7 +9,7 @@ resource "proxmox_vm_qemu" "k8s_node1" {
     force_create           = false
     full_clone             = false
     hotplug                = "network,disk,usb"
-    vmid                   = 260
+    vmid                   = 620
     kvm                    = true
     memory                 = 4096
     name                   = "k8s-node1"
@@ -20,15 +20,14 @@ resource "proxmox_vm_qemu" "k8s_node1" {
     scsihw                 = "virtio-scsi-pci"
     sockets                = 1
     tablet                 = true
-    target_node            = "pve2-mgt"
+    target_node            = "pve1-mgt"
     vcpus                  = 0
 
     disk {
         backup             = true
         cache              = "none"
         discard            = "on"
-        file               = "260/vm-260-disk-1.qcow2"
-        format             = "qcow2"
+        file               = "620/vm-620-disk-1.qcow2"
         iops               = 0
         iops_max           = 0
         iops_max_length    = 0
@@ -50,14 +49,14 @@ resource "proxmox_vm_qemu" "k8s_node1" {
         ssd                = 1
         storage            = "truenas1"
         type               = "scsi"
-        volume             = "truenas1:260/vm-260-disk-1.qcow2"
+        volume             = "truenas1:620/vm-620-disk-1.qcow2"
     }
 
     network {
         bridge    = "vmbr0"
         firewall  = true
         link_down = false
-        macaddr   = "aa:aa:ac:00:00:00"
+        macaddr   = "ca:fe:01:05:01:01"
         model     = "virtio"
         mtu       = 0
         queues    = 0
@@ -71,7 +70,7 @@ resource "proxmox_vm_qemu" "k8s_node1" {
 resource "proxmox_vm_qemu" "k8s_node2" {
     agent                  = 1
     bios                   = "ovmf"
-    boot                   = "order=ide2;scsi0;net0"
+    boot                   = "order=scsi0;ide2;net0"
     clone                  = "debian12-template"
     cores                  = 2
     cpu                    = "host"
@@ -79,7 +78,7 @@ resource "proxmox_vm_qemu" "k8s_node2" {
     force_create           = false
     full_clone             = false
     hotplug                = "network,disk,usb"
-    vmid                   = 261
+    vmid                   = 621
     kvm                    = true
     memory                 = 4096
     name                   = "k8s-node2"
@@ -90,15 +89,14 @@ resource "proxmox_vm_qemu" "k8s_node2" {
     scsihw                 = "virtio-scsi-pci"
     sockets                = 1
     tablet                 = true
-    target_node            = "pve2-mgt"
+    target_node            = "pve1-mgt"
     vcpus                  = 0
 
     disk {
         backup             = true
         cache              = "none"
         discard            = "on"
-        file               = "261/vm-261-disk-1.qcow2"
-        format             = "qcow2"
+        file               = "621/vm-621-disk-1.qcow2"
         iops               = 0
         iops_max           = 0
         iops_max_length    = 0
@@ -120,14 +118,14 @@ resource "proxmox_vm_qemu" "k8s_node2" {
         ssd                = 1
         storage            = "truenas1"
         type               = "scsi"
-        volume             = "truenas1:261/vm-261-disk-1.qcow2"
+        volume             = "truenas1:621/vm-621-disk-1.qcow2"
     }
 
     network {
         bridge    = "vmbr0"
         firewall  = true
         link_down = false
-        macaddr   = "aa:aa:ac:00:01:00"
+        macaddr   = "ca:fe:01:05:02:01"
         model     = "virtio"
         mtu       = 0
         queues    = 0
@@ -141,7 +139,7 @@ resource "proxmox_vm_qemu" "k8s_node2" {
 resource "proxmox_vm_qemu" "k8s_node3" {
     agent                  = 1
     bios                   = "ovmf"
-    boot                   = "order=ide2;scsi0;net0"
+    boot                   = "order=scsi0;ide2;net0"
     clone                  = "debian12-template"
     cores                  = 2
     cpu                    = "host"
@@ -149,7 +147,7 @@ resource "proxmox_vm_qemu" "k8s_node3" {
     force_create           = false
     full_clone             = false
     hotplug                = "network,disk,usb"
-    vmid                   = 262
+    vmid                   = 622
     kvm                    = true
     memory                 = 4096
     name                   = "k8s-node3"
@@ -160,15 +158,14 @@ resource "proxmox_vm_qemu" "k8s_node3" {
     scsihw                 = "virtio-scsi-pci"
     sockets                = 1
     tablet                 = true
-    target_node            = "pve2-mgt"
+    target_node            = "pve1-mgt"
     vcpus                  = 0
 
     disk {
         backup             = true
         cache              = "none"
         discard            = "on"
-        file               = "262/vm-262-disk-1.qcow2"
-        format             = "qcow2"
+        file               = "622/vm-622-disk-1.qcow2"
         iops               = 0
         iops_max           = 0
         iops_max_length    = 0
@@ -190,14 +187,14 @@ resource "proxmox_vm_qemu" "k8s_node3" {
         ssd                = 1
         storage            = "truenas1"
         type               = "scsi"
-        volume             = "truenas1:262/vm-262-disk-1.qcow2"
+        volume             = "truenas1:622/vm-622-disk-1.qcow2"
     }
 
     network {
         bridge    = "vmbr0"
         firewall  = true
         link_down = false
-        macaddr   = "aa:aa:ac:00:02:00"
+        macaddr   = "ca:fe:01:05:03:01"
         model     = "virtio"
         mtu       = 0
         queues    = 0
