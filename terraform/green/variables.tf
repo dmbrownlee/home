31037e54062648971ebc30e71ccc9480e4dad0a8
D. M. Brownlee <dmbrownlee@users.noreply.github.com>
2023-12-29 23:04:32 +0000

    Use terraform variables for connection arguments rather than environment variables.

---
+++ terraform/green/variables.tf
@@ -0,0 +1,17 @@
+#===========================================================
+# These variables are used by the Terraform Proxmox provider
+#===========================================================
+variable "pm_api_url" {
+  description = "URL of the Proxmox cluster API"
+  type        = string
+}
+
+variable "pm_api_token_secret" {
+  description = "Proxmox API token secret"
+  type        = string
+}
+
+variable "pm_api_token_id" {
+  description = "Proxmox API token ID"
+  type        = string
+}
