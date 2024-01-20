3d7b924a12af65b03aaee61caa656faa648679b2
D. M. Brownlee <dmbrownlee@users.noreply.github.com>
2024-01-20 22:01:23 +0000

    Initial commit using bpg terraform provider to spin up debian 12 cloud-init VMs for kubernetes practice.

---
+++ terraform/k8s/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = "~> 1.6.1"
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "0.43.2"
+    }
+  }
+}
+
+provider "proxmox" {
+  api_token = var.api_token
+  endpoint  = var.endpoint
+  insecure  = true
+  ssh {
+    agent    = true
+    username = "root"
+  }
+}
