36dbe60d8b805e0e440cf6e33239cfbc2895eb3f
D. M. Brownlee <dmbrownlee@users.noreply.github.com>
2024-03-04 17:29:59 +0000

    Move public key to a variable and update example config.

--- terraform/k8s/resources.tf
+++ terraform/k8s/resources.tf
@@ -42,7 +42,7 @@ resource "proxmox_virtual_environment_vm" "control_plane_nodes" {
     user_account {
       username = var.ciuser
       password = var.cipassword
-      keys     = ["ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCvfVK4u6FX5aWXcQIX3h0vQh+YywyJ3A+SV7soleU21f+7NZfkm9PybLnSFq5QbOdRZRe9kcLAGrGG8apNGTsZH2y25kt3HQm5PLS4E+jKg9An+fqk+4XMfRYWb71ZPZo5J8M5xOtL3VjNVS+9pDLeWhAw0OMlyl8+6QZnHg/+vOysJ4PebBiI7Nr2QjRh8h3hCB4xDe3f+U+GDML0A18pfgBS66y/JqKS+P+p2BsBnYLDZlAr/8Q3JWKfGra1yhftT3rEiZmIK+RT9qUeMgzyMC/rJa5lb7LJ2p4n3utsk90HlE0R4xDrSTk7ZkU/MxsHFmsc9JSx522DNXthlaz0U+GZgctBydWPYxAWdj2tjjniiJm4iZdt3pg4qPtr8fBWveyEzXuo6Yw66sA91Z6GOJA/ltmZjQUyh5d4M4NV+kFlydvEXHLVIfrUq51cDFGPHBhxkkIp32Lvrwvq45sKTiMbHt5xpTVHClrQE55SvJePNMtaJV6YDZ0zRJ9nonVULrIY+j/kNKSe7UtAdGWoNRyTGTi3O59pD3eRlBkFqxUV9fZ43TCBTNswubuF0RIyTyPqkOr0OGANq6mgp/Cuy0C9TE6FKWTyt9ygrZ3Xsw4B9gn6t2K96QgH8wO7rBtvJiB6liOwv8Thze8M3dphrBo4ka/IFRq/Ust8xvH3yQ=="]
+      keys     = var.cipubkey
     }
   }
   memory {
@@ -107,7 +107,7 @@ resource "proxmox_virtual_environment_vm" "worker_nodes" {
     user_account {
       username = var.ciuser
       password = var.cipassword
-      keys     = ["ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCvfVK4u6FX5aWXcQIX3h0vQh+YywyJ3A+SV7soleU21f+7NZfkm9PybLnSFq5QbOdRZRe9kcLAGrGG8apNGTsZH2y25kt3HQm5PLS4E+jKg9An+fqk+4XMfRYWb71ZPZo5J8M5xOtL3VjNVS+9pDLeWhAw0OMlyl8+6QZnHg/+vOysJ4PebBiI7Nr2QjRh8h3hCB4xDe3f+U+GDML0A18pfgBS66y/JqKS+P+p2BsBnYLDZlAr/8Q3JWKfGra1yhftT3rEiZmIK+RT9qUeMgzyMC/rJa5lb7LJ2p4n3utsk90HlE0R4xDrSTk7ZkU/MxsHFmsc9JSx522DNXthlaz0U+GZgctBydWPYxAWdj2tjjniiJm4iZdt3pg4qPtr8fBWveyEzXuo6Yw66sA91Z6GOJA/ltmZjQUyh5d4M4NV+kFlydvEXHLVIfrUq51cDFGPHBhxkkIp32Lvrwvq45sKTiMbHt5xpTVHClrQE55SvJePNMtaJV6YDZ0zRJ9nonVULrIY+j/kNKSe7UtAdGWoNRyTGTi3O59pD3eRlBkFqxUV9fZ43TCBTNswubuF0RIyTyPqkOr0OGANq6mgp/Cuy0C9TE6FKWTyt9ygrZ3Xsw4B9gn6t2K96QgH8wO7rBtvJiB6liOwv8Thze8M3dphrBo4ka/IFRq/Ust8xvH3yQ=="]
+      keys     = var.cipubkey
     }
   }
   memory {
