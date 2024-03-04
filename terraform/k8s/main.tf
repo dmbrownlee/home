0e0f585ce93f23155404b54947f7da11f4564b79
D. M. Brownlee <dmbrownlee@users.noreply.github.com>
2024-03-04 17:30:57 +0000

    Update terraform and plugin versions.

--- terraform/k8s/main.tf
+++ terraform/k8s/main.tf
@@ -1,13 +1,13 @@
 terraform {
-  required_version = "~> 1.7.1"
+  required_version = "~> 1.7.4"
   required_providers {
     proxmox = {
       source  = "bpg/proxmox"
-      version = "0.43.2"
+      version = "0.48.0"
     }
     ansible = {
       source  = "ansible/ansible"
-      version = "1.1.0"
+      version = "1.2.0"
     }
   }
 }
