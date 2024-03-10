a02073eae52bf8eab683026afe506ca3360dc811
D. M. Brownlee <dmbrownlee@users.noreply.github.com>
2024-03-10 21:19:17 +0000

    Add the minikube node to the combined Terraform project.

---
+++ terraform/k8s/vms-minikube.tf
@@ -0,0 +1,70 @@
+locals {
+  minikube_vlan = var.vlans[index(var.vlans.*.comment, "DMZ")]
+}
+
+resource "proxmox_virtual_environment_vm" "minikube" {
+  depends_on  = [proxmox_virtual_environment_vm.vm_templates]
+  for_each    = { for n in var.minikube_nodes:
+                    n.hostname => {
+                      pve_node = n.pve_node,
+                      vm_id = n.vm_id,
+                      cloud_init_image = n.cloud_init_image,
+                      mac_address = n.mac_address,
+                      ipv4_address = n.ipv4_address
+                    }
+                }
+  name        = each.key
+  description = "Managed by Terraform"
+  tags        = [each.value.cloud_init_image, "minikube", "terraform"]
+  node_name   = each.value.pve_node
+  vm_id       = each.value.vm_id
+
+  clone {
+    datastore_id = "truenas1"
+    node_name = "pve3"
+    vm_id        = var.vm_templates[each.value.cloud_init_image].vm_id
+    full = true
+  }
+  cpu {
+    cores = 8
+  }
+  initialization {
+    datastore_id = "truenas1"
+    dns {
+      servers = local.minikube_vlan.ipv4_dns_servers
+    }
+    ip_config {
+      ipv4 {
+        address = each.value.ipv4_address
+        gateway = local.minikube_vlan.ipv4_gateway
+      }
+    }
+    user_account {
+      username = var.ciuser
+      password = var.cipassword
+      keys     = var.cipubkey
+    }
+  }
+  memory {
+    dedicated = 16384
+    floating = 16384
+  }
+  network_device {
+    bridge      = "vmbr0"
+    mac_address = each.value.mac_address
+    vlan_id     = local.minikube_vlan.vlan_id
+  }
+  on_boot = false
+  connection {
+    type     = "ssh"
+    user     = var.ciuser
+    agent    = true
+    host     = self.name
+  }
+  provisioner "remote-exec" {
+    inline = [ "ip a" ]
+  }
+  vga {
+    type = "qxl"
+  }
+}
