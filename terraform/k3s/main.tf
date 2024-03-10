f2eb25d2542baa62b9f25b113935cd9c7dc39d42
D. M. Brownlee <dmbrownlee@users.noreply.github.com>
2024-03-10 22:32:00 +0000

    Update terraform version and merge main.tf and provider.tf as part of deprecating separate k3s directory.

--- terraform/k3s/main.tf
+++ terraform/k3s/main.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "~> 1.6.1"
+  required_version = "~> 1.7.4"
   required_providers {
     proxmox = {
       source  = "TheGameProfi/proxmox"
@@ -9,5 +9,19 @@ terraform {
       source  = "ansible/ansible"
       version = "1.1.0"
     }
+  }
+}
+
+provider "proxmox" {
+  pm_api_url          = var.pm_api_url
+  pm_api_token_secret = var.pm_api_token_secret
+  pm_api_token_id     = var.pm_api_token_id
+  pm_tls_insecure     = true
+  pm_log_enable       = true
+  pm_log_file         = "terraform-plugin-proxmox.log"
+  pm_debug            = true
+  pm_log_levels = {
+    _default    = "debug"
+    _capturelog = ""
   }
 }
