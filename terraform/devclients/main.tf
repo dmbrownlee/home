0a8cd1b20291cfef2bc107fe297eede833bac067
D. M. Brownlee <dmbrownlee@users.noreply.github.com>
2024-06-22 22:12:50 +0000

    Migrate devclients from telmate to bpg proxmox provider.

--- terraform/devclients/main.tf
+++ terraform/devclients/main.tf
@@ -1,9 +1,27 @@
 terraform {
-  required_version = ">= 1.5.5"
+  required_version = ">= 1.7.2"
   required_providers {
-    proxmox = {
-      source  = "TheGameProfi/proxmox"
-      version = "2.9.15"
+    ansible = {
+      source  = "ansible/ansible"
+      version = "1.3.0"
     }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.5.1"
+    }
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "0.60.0"
+    }
+  }
+}
+
+provider "proxmox" {
+  api_token = var.api_token
+  endpoint  = var.endpoint
+  insecure  = true
+  ssh {
+    agent    = true
+    username = "root"
   }
 }
