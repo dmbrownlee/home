1dc2e6a829c607e4161bb5cd960020d385b46cf9
D. M. Brownlee <dmbrownlee@users.noreply.github.com>
2024-07-14 16:04:47 +0000

    Create directory for opentofu projects and start with isolated VLAN provisioning.

---
+++ tofu/vlans/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_version = ">= 1.7.2"
+  required_providers {
+    ansible = {
+      source  = "ansible/ansible"
+      version = "1.3.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.5.1"
+    }
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "0.61.1"
+    }
+  }
+}
+
+provider "proxmox" {
+  api_token = var.api_token
+  endpoint  = var.endpoint
+  insecure  = true
+  ssh {
+    agent    = true
+    username = "root"
+  }
+}
