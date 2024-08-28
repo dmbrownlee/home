28a9e8c0292615b3adbb258de8315668dc55888b
D. M. Brownlee <dmbrownlee@users.noreply.github.com>
2024-08-28 22:49:18 +0000

    A separate project for a development cluster is not needed when using workspaces.

--- tofu/lab-k8s/data.tf
+++
@@ -1,9 +0,0 @@
-data "proxmox_virtual_environment_nodes" "available_nodes" {}
-
-data "proxmox_virtual_environment_vms" "cloud_init_template" {
-  tags = ["cloud_init_template"]
-}
-
-data "local_file" "ci_ssh_public_key_file" {
-  filename = pathexpand("${var.ssh_keystore}/${var.ci_user}.pub")
-}
